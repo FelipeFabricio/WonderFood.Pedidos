be9988d332508a2bc58286d2a83fb4c8e5ac2833
FelipeFabricio <ff.oliveira32@gmail.com>
2024-01-21 12:31:02 -0300

    Remove dto não utilizada

--- src/WonderFood.Core/Dtos/Pedido/AvancarStatusPedidoDto.cs
+++
@@ -1,9 +0,0 @@
-using WonderFood.Core.Entities.Enums;
-
-namespace WonderFood.Core.Dtos;
-
-public class AvancarStatusPedidoDto
-{
-    public StatusPedido Status { get; set; }
-    public int NumeroPedido { get; set; }
-}
