16fae468131f276285902d2b46e43d1fb19cc0f7
FelipeFabricio <ff.oliveira32@gmail.com>
2024-03-10 19:07:12 -0300

    Atualiza Schema de dados

--- src/WonderFood.Infra.Sql/Mappings/ProdutoDatabaseMapping.cs
+++ src/WonderFood.Infra.Sql/Mappings/ProdutoDatabaseMapping.cs
@@ -10,10 +10,10 @@ public class ProdutoDatabaseMapping : IEntityTypeConfiguration<Produto>
     {
         builder.ToTable("Produtos");
         builder.HasKey(p => p.Id);
-        builder.Property(p => p.Id).HasColumnType("int").IsRequired();
+        builder.Property(p => p.Id).HasColumnType("varchar(36)").IsRequired();
         builder.Property(p => p.Nome).HasColumnType("varchar(100)").IsRequired();
         builder.Property(p => p.Descricao).HasColumnType("varchar(200)").IsRequired(false);
         builder.Property(p => p.Valor).HasColumnType("decimal(8,2)").IsRequired();
-        builder.Property(p => p.Categoria).HasConversion<short>().IsRequired();
+        builder.Property(p => p.Categoria).HasConversion<byte>().IsRequired();
     }
 }
---
+++ src/WonderFood.Infra.Sql/Migrations/20240310220327_AlteraColunasShortParaTinyint.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WonderFood.Infra.Sql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteraColunasShortParaTinyint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Quantidade",
+                table: "ProdutosPedido",
+                type: "tinyint unsigned",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Categoria",
+                table: "Produtos",
+                type: "tinyint unsigned",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Status",
+                table: "Pedidos",
+                type: "tinyint unsigned",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantidade",
+                table: "ProdutosPedido",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint unsigned");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "Categoria",
+                table: "Produtos",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint unsigned");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "Status",
+                table: "Pedidos",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint unsigned");
+        }
+    }
+}
