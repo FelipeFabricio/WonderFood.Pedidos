188d4ee940fdbe9f2b15d73c9310d3ce6b37f90d
Felipe Fabricio <ff.oliveira32@gmail.com>
2024-04-25 00:34:03 -0300

    Delete src/Wonderfood.Models directory

--- src/Wonderfood.Models/Events/PagamentoProcessadoEvent.cs
+++
@@ -1,9 +0,0 @@
-using WonderFood.Models.Enums;
-
-namespace WonderFood.Models.Events;
-
-public class PagamentoProcessadoEvent
-{
-    public Guid IdPedido { get; set; }
-    public SituacaoPagamento StatusPagamento { get; set; }
-}
