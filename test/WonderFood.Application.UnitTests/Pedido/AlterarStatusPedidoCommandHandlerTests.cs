911893e01d7f149a54cb20692f55e9e8f1666251
Felipe Fabricio <ff.oliveira32@gmail.com>
2024-06-16 23:58:24 -0300

    Descomenta testes unitários

--- test/WonderFood.Application.UnitTests/Pedido/AlterarStatusPedidoCommandHandlerTests.cs
+++ test/WonderFood.Application.UnitTests/Pedido/AlterarStatusPedidoCommandHandlerTests.cs
@@ -1,13 +1,11 @@
 using FluentAssertions;
 using MediatR;
 using NSubstitute;
-using NSubstitute.ExceptionExtensions;
 using NSubstitute.ReturnsExtensions;
 using WonderFood.Application.Common.Interfaces;
 using WonderFood.Application.Pedidos.Commands.AlterarStatus;
 using WonderFood.Application.UnitTests.Pedido.Factory;
 using WonderFood.Domain.Entities.Enums;
-using WonderFood.Models.Events;
 using Xunit;
 
 namespace WonderFood.Application.UnitTests.Pedido;
@@ -22,47 +20,38 @@ public class AlterarStatusPedidoCommandHandlerTests
     {
         _sut = new AlterarStatusPedidoCommandHandler(_pedidoRepository, _unitOfWork);
     }
-    //
-    // [Fact]
-    // [Trait("Application", "Pedidos")]
-    // public async Task Handle_DeveAtualizar_QuandoPedidoExistirNaBaseDeDados()
-    // {
-    //     //Arrange
-    //     var command = new AlterarStatusPedidoCommand(new AlteracaoStatusEvent
-    //     {
-    //         NumeroPedido = 1,
-    //         Status = StatusPedido.PagamentoAprovado
-    //     });
-    //     
-    //     var pedidoEntity = PedidosFactory.CriarPedidoEntity();
-    //     _pedidoRepository.ObterPorNumeroPedido(Arg.Any<int>()).Returns(pedidoEntity);
-    //     _pedidoRepository.AtualizarStatus(pedidoEntity).Returns(Task.CompletedTask);
-    //     
-    //     //Act
-    //     var result = await _sut.Handle(command, CancellationToken.None);
-    //     
-    //     //Assert
-    //     result.Should().NotBeNull();
-    //     result.Should().BeAssignableTo<Unit>();
-    // }
-    //
-    // [Fact]
-    // [Trait("Application", "Pedidos")]
-    // public async Task Handle_DeveLancarArgumentException_QuandoPedidoNaoExistirNaBaseDeDados()
-    // {
-    //     //Arrange
-    //     var command = new AlterarStatusPedidoCommand(new AlteracaoStatusEvent
-    //     {
-    //         NumeroPedido = 1,
-    //         Status = StatusPedido.PagamentoAprovado
-    //     });
-    //
-    //     _pedidoRepository.ObterPorNumeroPedido(Arg.Any<int>()).ReturnsNull();
-    //     
-    //     //Act
-    //     Func<Task> action = async () => await  _sut.Handle(command, CancellationToken.None);
-    //     
-    //     //Assert
-    //     await action.Should().ThrowAsync<ArgumentException>();
-    // }
+    
+    [Fact]
+    [Trait("Application", "Pedidos")]
+    public async Task Handle_DeveAtualizar_QuandoPedidoExistirNaBaseDeDados()
+    {
+        //Arrange
+        var command = new AlterarStatusPedidoCommand(Guid.NewGuid(), StatusPedido.PagamentoAprovado);
+        
+        var pedidoEntity = PedidosFactory.CriarPedidoEntity();
+        _pedidoRepository.ObterPorId(Arg.Any<Guid>()).Returns(pedidoEntity);
+        _pedidoRepository.AtualizarStatus(pedidoEntity).Returns(Task.CompletedTask);
+        
+        //Act
+        var result = await _sut.Handle(command, CancellationToken.None);
+        
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeAssignableTo<Unit>();
+    }
+    
+    [Fact]
+    [Trait("Application", "Pedidos")]
+    public async Task Handle_DeveLancarArgumentException_QuandoPedidoNaoExistirNaBaseDeDados()
+    {
+        //Arrange
+        var command = new AlterarStatusPedidoCommand(Guid.NewGuid(), StatusPedido.PagamentoAprovado);
+        _pedidoRepository.ObterPorNumeroPedido(Arg.Any<int>()).ReturnsNull();
+        
+        //Act
+        Func<Task> action = async () => await  _sut.Handle(command, CancellationToken.None);
+        
+        //Assert
+        await action.Should().ThrowAsync<ArgumentException>();
+    }
 }
